4151ffb7e816e045e56ae0276bae191ce73d9824
Yuzer <yusing.wys@gmail.com>
2020-11-11 13:49:14 +0800

    added script

---
+++ build_imgui.bat
@@ -0,0 +1,6 @@
+@echo off
+devenv imgui_lib\imgui_lib.sln /upgrade
+msbuild imgui_lib\imgui_lib.sln -p:Configuration=Release /p:Platform="x86"
+msbuild imgui_lib\imgui_lib.sln -p:Configuration=Release /p:Platform="x64"
+msbuild imgui_lib\imgui_lib.sln -p:Configuration=Debug /p:Platform="x86"
+msbuild imgui_lib\imgui_lib.sln -p:Configuration=Debug /p:Platform="x64"
